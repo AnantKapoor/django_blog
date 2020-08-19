b31ee7ed3645df0bec1aeb035b9e8428e5c27074
Anant Kapoor <anant.kapoor@outlook.com>
2020-08-19 19:16:50 +0100

    debug mode on?

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '18ulamtc3^_hqwitr$&tq_&2a1$5bi_yyi09e8##ruhv+gf)_e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
