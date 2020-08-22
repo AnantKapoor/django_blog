48dd0bb68f97a6f92a166a42526db9a954fd06c7
Anant Kapoor <anant.kapoor@outlook.com>
2020-08-22 15:54:57 +0100

    added CV template + started testing

--- cv/tests.py
+++ cv/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
+from cv.views import cv1
+from django.http import HttpRequest
 
 # Create your tests here.
+
+
+class HomePageTest(TestCase):
+
+       def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = cv1(request)  
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))  
+        self.assertIn('<title>CV</title>', html)  
+        self.assertTrue(html.endswith('</html>'))  
--- cv/views.py
+++ cv/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Education, Experience, Skills, Interests
 
 # Create your views here.
+def cv1(request):
+    education = Education.objects.all()
+    experience = Experience.objects.all()
+    skills = Skills.objects.all()
+    interests = Interests.objects.all()
+
+    return render(request, 'cv/cv1.html',{'education':education, 'experience':experience, 'skills':skills, 'interests':interests,})
