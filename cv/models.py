b578f5ecdc77e4b5a34eb0a13829b59b59dc7c90
Anant Kapoor <anant.kapoor@outlook.com>
2020-08-22 11:32:22 +0100

    created CV models

--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Education(models.Model):
+    title = models.CharField(max_length=200)
+    grades = models.CharField(max_length=200)
+    description = models.TextField(default=None)
+    graduation_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class Experience(models.Model):
+    title = models.CharField(max_length=200)
+    position = models.TextField()
+    text = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class Skills(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(default=None)
+
+    def __str__(self):
+        return self.title
+
+class Interests(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
